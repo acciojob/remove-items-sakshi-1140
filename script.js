747961d6c1a9b2d3276c0ebbb324ae79e1a3cb87
Sakshi Choudhary <103510144+sakshi-1140@users.noreply.github.com>
2024-06-03 18:28:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+const btn=document.querySelector('input[type="button"][value="Select and Remove"]')
+btn.addEventListener("click", function(){
+  const colorSelect=document.getElementById('colorSelect')
+  const selectedColorIndex=colorSelect.selectedIndex
+  colorSelect.remove(selectedColorIndex)
+})
